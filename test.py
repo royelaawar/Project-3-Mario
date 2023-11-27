6d13003b94cb2c41577b81c9a2a11a21b411f60f
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-27 16:50:59 -0500

    test bg

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ screen_height = 900
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
-bg_img = pygame.image.load('images/sky0.png')
-sun_img = pygame.image.load('images/sun1.png')
+bg_img = pygame.image.load('images/sky-4.png')
+sun_img = pygame.image.load('images/bomb.png')
 
 run = True
 while run:
@@ -20,7 +20,7 @@ while run:
 
     # Blit the background image onto the screen
     screen.blit(bg_img, (0, 0))
-    screen.blit(sun_img, (500, 0))
+    screen.blit(sun_img, (1300, 50))
 
     # Update the display
     pygame.display.flip()
