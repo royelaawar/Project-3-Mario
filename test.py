b88d903a7c016df8beccbe61c7403fe6c90e78d0
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-27 14:11:24 -0500

    add bg

--- test.py
+++ test.py
@@ -9,15 +9,18 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
-sun_img = pygame.image.load('./images/sun1.png')
-bg_img = pygame.image.load('./images/sky-3.png')
+bg_img = pygame.image.load('images/sky2.png')
 
 run = True
 while run:
-
-    screen.blit(bg_img(0, 0))
-    screen.blit(sun_img(100, 100))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # Blit the background image onto the screen
+    screen.blit(bg_img, (0, 0))
+
+    # Update the display
+    pygame.display.flip()
+
+pygame.quit()
