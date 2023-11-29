170372fccff92529fe9d3daba91a5a1fcc4bf680
Oliver Gray <olliewgray@gmail.com>
2023-11-29 16:21:55 -0500

    merged roy's main with oliver's

--- game/Mario.py
+++ game/Mario.py
@@ -13,16 +13,26 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Mario')
 
+#define font + font colors
+game_over_font = pygame.font.SysFont('Bauhaus 93', 80)
+font_score = pygame.font.SysFont('Bauhaus 93', 40)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
 #define game variables
 tile_size = 50
 game_over = 0
-current_level = 0 
+main_menu = True
+score = 0
+current_level = 0
+
+ 
 levels = [
 
     [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+    [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
     [1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 4, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -36,8 +46,8 @@ levels = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
     [1, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 1, 1, 8, 8, 8, 1],
+    [1, 0, 0, 0, 0, 1, 8, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ],
@@ -65,18 +75,33 @@ levels = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ],
 ]
-#load images
 
+#load images
 bg_img = pygame.image.load('game/img/sky.png')
 restart_img = pygame.image.load('game/img/restart.png')
+start_img = pygame.image.load('game/img/start.png')
+stop_img = pygame.image.load('game/img/stop.png')
+game_over_img = pygame.image.load('game/img/game_over_alt.png')
+game_title_img = pygame.image.load('game/img/game_title.png')
+
+## renders text on screen as image
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 class Button():
     def __init__(self, x, y, image):
         self.image = image
+        #scaling the original images down to 1/4 size
+        original_size = self.image.get_size()
+        new_size = (original_size[0] // 2, original_size[1] // 2) 
+        scaled_image = pygame.transform.scale(self.image, new_size)
+        self.image = scaled_image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.clicked = False
+
     
     def draw(self):
         action = False
@@ -106,7 +131,7 @@ class Player():
        
             key = pygame.key.get_pressed()
             if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
-                self.vel_y = -15
+                self.vel_y = -30
                 self.jumped = True
             if key[pygame.K_SPACE] == False:
                 self.jumped = False
@@ -139,9 +164,18 @@ class Player():
                         self.vel_y = 0
                         self.in_air = False
 
+            ##COLLISIONS:
+            #check for collisions w/ enemies
             if pygame.sprite.spritecollide(self, enemy_group, False):
                 game_over = -1
-            
+                
+            #check for collisions w/ spikes and lava
+            if pygame.sprite.spritecollide(self, spike_group, False):
+                game_over = -1
+            if pygame.sprite.spritecollide(self, lava_group, False):
+                game_over = -1
+                
+            ## collision w/ exit sprite moves char to next level
             if pygame.sprite.spritecollide(self, exit_group, False):
                 game_over = 1
 
@@ -150,7 +184,8 @@ class Player():
             self.rect.y += dy
 
         elif game_over == -1:
-            self.image = self.image
+            dead_image = pygame.image.load('game/img/ghost.png')
+            self.image = pygame.transform.scale(dead_image, (65, 90))
             self.rect.y -= 5
 
         #draw player onto the screen
@@ -174,7 +209,7 @@ class World():
         self.tile_list = []
 
         #load images
-        grass_img = pygame.image.load('game/img/dirt.png')
+        grass_img = pygame.image.load('game/img/ground_grassy_1.png')
         brick_img = pygame.image.load('game/img/brick.png')
         
 
@@ -202,6 +237,17 @@ class World():
                 if tile == 5:
                     exit = Exit(col_count * tile_size, row_count * tile_size -200)
                     exit_group.add(exit)
+                if tile == 6:
+                    spike = Spike(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+                    spike_group.add(spike)
+                if tile == 7:
+                    coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+                    coin_group.add(coin)
+                if tile == 8:
+                    lava = Lava(col_count * tile_size, row_count * tile_size )
+                    lava_group.add(lava)
+                    
+            
                 col_count += 1
             row_count += 1
 
@@ -224,6 +270,7 @@ class Enemy(pygame.sprite.Sprite):
     
     def update(self):
 
+
         self.rect.x += self.move_direction
         self.move_counter += 1
         if abs(self.move_counter)  > 50:
@@ -240,74 +287,105 @@ class Exit(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
+class Spike(pygame.sprite.Sprite):
+   def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('game/img/spike_1.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-# world_data = [
-#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
-#     [1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 5, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 4, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 7, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
-#     [1, 0, 2, 2, 2, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 3, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
-#     [1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
-#     [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#     [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-# ]
+class Lava(pygame.sprite.Sprite):
+   def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('game/img/lava_1.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size ))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+class Coin(pygame.sprite.Sprite):
+   def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('game/img/coin_gold.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
+
+
+## player instance
 player = Player(100, screen_height - 130)
 
+## sprite groups
 enemy_group = pygame.sprite.Group()
+spike_group = pygame.sprite.Group()
+lava_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
 
 world = World(levels[current_level])
 
-restart_button = Button (screen_width // 2 - 350, screen_height // 2 -200, restart_img)
 
+## interface buttons
+game_title = Button(screen_width // 2 - 150, screen_height // 2 - 375, game_title_img)
+restart_button = Button(screen_width // 2 - 250, screen_height // 2 - 100, game_over_img)
+start_button = Button(screen_width // 2 - 350, screen_height // 2 - 50, start_img)
+stop_button = Button(screen_width // 2 , screen_height // 2 - 50, stop_img)
+score_count_coin = Coin(tile_size // 2, tile_size // 2)
+coin_group.add(score_count_coin)
+
+
+### GAME LOOP
 run = True
 while run:
 
     clock.tick(fps)
     screen.blit(bg_img, (0, 0))
     
-    world.draw()
-    
-    if game_over == 0:
-        enemy_group.update()
-
-    enemy_group.update()
-    enemy_group.draw(screen)
-    exit_group.draw(screen)
-
-    game_over = player.update(game_over)
-
-    if game_over == -1:
-        if restart_button.draw():
-            player.reset(100, screen_height - 130)
-            game_over = 0
-            
-    if game_over == 1:
-        current_level += 1
-        if current_level < len(levels):
-            # Load the next level
-            world = World(levels[current_level])
-            player.reset(100, screen_height - 130)
-            game_over = 0
-        else:
-            # The player has completed all levels, you can handle this as needed
+    if main_menu == True:
+        game_title.draw()
+        if stop_button.draw():
             run = False
+        if start_button.draw():
+            main_menu = False
+    else:
+        world.draw()
+        
+        if game_over == 0:
+            enemy_group.update()
+            if pygame.sprite.spritecollide(player, coin_group, True):
+                score += 1
+            draw_text('X ' + str(score), font_score, white, ((tile_size // 2) + 15), ((tile_size // 2) - 10))
+            
+        enemy_group.update()
+        enemy_group.draw(screen)
+        spike_group.draw(screen)
+        coin_group.draw(screen)
+        lava_group.draw(screen)
+        exit_group.draw(screen)
+        game_over = player.update(game_over)
 
+        ## if player has died
+        if game_over == -1:
+            # draw_text('GAME OVER!', game_over_font, red, (screen_width // 2) - 250, (screen_width // 2))
+            if restart_button.draw():
+                player.reset(100, screen_height - 130)
+                game_over = 0
+                score = 0
+        if game_over == 1:
+            current_level += 1
+            if current_level < len(levels):
+                # Load the next level
+                world = World(levels[current_level])
+                player.reset(100, screen_height - 130)
+                game_over = 0
+            else:
+                # The player has completed all levels, you can handle this as needed
+                run = False
 
-    # draw_grid()
+# draw_grid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
